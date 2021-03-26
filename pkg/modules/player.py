e92629e3dfc91ab09ef157a63a8d7338ec659fe9
David José Cotilla Martín <36771356+coti92@users.noreply.github.com>
2021-03-26 14:19:53 +0100

    Add files via upload

---
+++ pkg/modules/player.py
@@ -0,0 +1,25 @@
+import random
+from pkg.modules.labelconstans import LabelConstants
+
+class Player(object):
+    
+    def __init__(self):
+        self.hand = []
+
+    def player_hand(self):
+        print('Please choose (R)ock, (P)aper, or (S)cissor: ')
+        hand_p = str(input()).upper()
+        if hand_p in LabelConstants.LABELS.keys():
+            return LabelConstants.LABELS[hand_p]
+        else:
+            print ('Invalid selection. Please try again.')
+            self.player_hand()
+
+    def computer_hand(self):
+        hand_c = random.randint(1,3)
+        if hand_c == 1:
+            return LabelConstants.LABELS["R"]
+        elif hand_c == 2:
+            return LabelConstants.LABELS["P"]
+        elif hand_c == 3:
+            return LabelConstants.LABELS["S"]
